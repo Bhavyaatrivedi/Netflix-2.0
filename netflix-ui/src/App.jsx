c1e674ab4b0441b0a0542df9965081d01e1e739d
Bhavyaa Trivedi <bhavyaatrivedi2002@gmail.com>
2023-07-25 19:29:51 +0530

    movies page

--- netflix-ui/src/App.jsx
+++ netflix-ui/src/App.jsx
@@ -14,10 +14,10 @@ export default function App() {
       <Routes>
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/signup" element={<Signup />} />
-        {/* <Route exact path="/player" element={<Player />} />
-        <Route exact path="/tv" element={<TVShows />} />
-        <Route exact path="/movies" element={<MoviePage />} />
-        <Route exact path="/new" element={<Player />} />
+        <Route exact path="/player" element={<Player />} />
+        {/* <Route exact path="/tv" element={<TVShows />} /> */}
+        <Route exact path="/movies" element={<Movies />} />
+        {/* <Route exact path="/new" element={<Player />} />
         <Route exact path="/mylist" element={<UserListedMovies />} /> */}
         <Route exact path="/" element={<Netflix />} />
       </Routes>
