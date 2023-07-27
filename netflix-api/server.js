125110d4070658a88f39bd33886bbba9216506cc
Bhavyaa Trivedi <bhavyaatrivedi2002@gmail.com>
2023-07-27 09:45:00 +0530

    backend setup

---
+++ netflix-api/server.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+//const userRoutes = require("./routes/UserRoutes");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    
+    })
+    .then((data) => {
+      console.log("Connected to DB");
+    });
+};
+
+connectDatabase();
+
+// app.use("/api/user", userRoutes);
+
+app.listen(5000, () => {
+  console.log("server started on port 5000");
+});
--- netflix-ui/src/utils/constants.js
+++
@@ -1,2 +0,0 @@
-export const API_KEY = "e8d8c205f72f233494124b15832569fd";
-export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
